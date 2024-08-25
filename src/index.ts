b1deffb24a113029fc4c7d67cbf3a69c1572ff45
Antony74 <akb@akb.me.uk>
2024-08-25 11:00:46 +0100

    Start drawing maze

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,64 @@
 import p5 from 'p5';
 import { createGraphOfGrid } from './graph/graphOfGrid';
 
+type MazeEdgeProps = { wall: boolean };
+
 new p5((p: p5) => {
-    const { graph } = createGraphOfGrid({
-        xSize: 2,
-        ySize: 2,
-        initialEdgeProps: {},
+    const xSize = 10;
+    const ySize = 10;
+
+    const line = (from: p5.Vector, to: p5.Vector) =>
+        p.line(from.x, from.y, to.x, to.y);
+
+    const { grid } = createGraphOfGrid<MazeEdgeProps>({
+        xSize,
+        ySize,
+        initialEdgeProps: { wall: true },
     });
 
-    for (const vertex of graph.vertices) {
-        console.log(`vertex (${vertex.x}, ${vertex.y}) leads`);
-        let index = 0;
-        for (const edge of vertex.edges) {
-            const connectedVertex = graph.followEdge(vertex, edge);
-            console.log(
-                `    ${vertex.directions[index]} to vertex (${connectedVertex.x}, ${connectedVertex.y})`
-            );
-            ++index;
-        }
-    }
+    const getVertexPosition = (xIndex: number, yIndex: number) => {
+        const x = p.map(xIndex, -1, xSize, 0, p.width);
+        const y = p.map(yIndex, -1, ySize, 0, p.height);
+        return new p5.Vector(x, y);
+    };
 
     p.setup = () => {
-        p.ellipse(0.5 * p.width, 0.5 * p.height, 0.9 * p.width, 0.9 * p.height);
+        p.ellipseMode(p.RADIUS);
+        p.createCanvas(300, 300);
+    };
+
+    p.draw = () => {
+        p.background(255);
+        p.fill(0, 255, 255);
+
+        for (let y = 0; y < ySize; ++y) {
+            for (let x = 0; x < xSize; ++x) {
+                const vertex = grid[x][y];
+                if (vertex) {
+                    const vertexPosition = getVertexPosition(x, y);
+                    p.noStroke();
+                    p.ellipse(vertexPosition.x, vertexPosition.y, 5);
+                    p.stroke(0);
+
+                    const leftVertex = (grid[x - 1] ?? [])[y];
+                    const rightVertex = (grid[x + 1] ?? [])[y];
+
+                    const leftEdgeIndex = vertex.directions.findIndex(
+                        (direction) => direction === 'left'
+                    );
+
+                    const leftEdge = vertex.edges[leftEdgeIndex];
+
+                    if (!leftVertex || !leftEdge || leftEdge.wall) {
+                        line(
+                            getVertexPosition(x - 0.5, y - 0.5),
+                            getVertexPosition(x - 0.5, y + 0.5)
+                        );
+                    }
+                }
+            }
+        }
+
+        p.noLoop();
     };
 });
