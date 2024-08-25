58c4ee656f8fb8a990b08691d9ab718ea0b8fa2a
Antony74 <akb@akb.me.uk>
2024-08-25 20:30:25 +0100

    Find a pleasing layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,52 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { Sketch } from './sketch';
-import { MenuItem, Select } from '@mui/material';
+import { Button, Grid, MenuItem, TextField } from '@mui/material';
 import GitHubForkRibbon from 'react-github-fork-ribbon';
 
-const Controls = () => (
-    <div>
-        <Select label="Mode">
-            <MenuItem>Plain</MenuItem>
-            <MenuItem>Step through</MenuItem>
-        </Select>
-    </div>
-);
+const margin = 10;
+const padding = 50;
 
-const App = () => (
-    <div>
-        {' '}
-        <GitHubForkRibbon
-            href="https://github.com/Antony74/maze"
-            target="_blank"
-            position="right"
-        >
-            Fork me on GitHub
-        </GitHubForkRibbon>
-        <Sketch></Sketch>
-        <Controls></Controls>
-    </div>
-);
+const Controls = () => {
+    return (
+        <Grid container justifyContent="center">
+            <TextField
+                select
+                label="Mode"
+                size="small"
+                value={'plain'}
+                style={{ width: '200px', margin }}
+            >
+                <MenuItem key={1} value={'plain'}>
+                    Plain
+                </MenuItem>
+                <MenuItem key={2} value={'step'}>
+                    Step through
+                </MenuItem>
+            </TextField>
+            <Button variant="outlined" style={{ margin }}>
+                Reset
+            </Button>
+        </Grid>
+    );
+};
+
+const App = () => {
+    return (
+        <div>
+            <GitHubForkRibbon
+                href="https://github.com/Antony74/maze"
+                target="_blank"
+                position="right"
+            >
+                Fork me on GitHub
+            </GitHubForkRibbon>
+            <Grid container justifyContent="center" style={{ padding }}>
+                <Sketch></Sketch>
+                <Controls></Controls>
+            </Grid>
+        </div>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
